80311e1f7eb56148d6c588640fc5aa153447d114
VennV <hyplex9009@gmail.com>
2025-07-13 04:02:20 +0700

    Update!

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,8 +5,36 @@
 ---@type ChadrcConfig
 local M = {}
 
+M.ui = {
+  statusline = {
+    theme = "vscode_colored",
+    separator_style = "default",
+    overriden_modules = function()
+      local st_modules = require "nvchad_ui.statusline.default"
+      return {
+        fileInfo = function()
+          local fn = vim.fn
+          local sep = "%#St_file_sep#"
+          local icon = "  "
+          local filename = (fn.expand "%" == "" and "Empty ") or fn.expand "%:t"
+          local modified_indicator = vim.bo.modified and " " or ""
+          local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+          if devicons_present then
+            local ft_icon = devicons.get_icon(filename)
+            icon = (ft_icon ~= nil and " " .. ft_icon) or ""
+          end
+          return icon .. filename .. modified_indicator
+        end,
+        git = st_modules.git,
+        LSP_Diagnostics = st_modules.LSP_Diagnostics,
+        cursor_position = st_modules.cursor_position,
+      }
+    end,
+  },
+}
+
 M.base46 = {
-	theme = "rosepine",
+	theme = "github_dark",
 	transparency = true,
 
 	-- hl_override = {
