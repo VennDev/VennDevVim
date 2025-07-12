fc43ff922f5ae694d36d5018b88c809094fe9127
VennV <hyplex9009@gmail.com>
2025-07-13 03:50:14 +0700

    Update!

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,8 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
